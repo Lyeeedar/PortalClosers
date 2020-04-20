7c914b029fa01104a83b7081386f705e39b43f60
lyeeedar <lyeeedar@gmail.com>
2020-04-20 22:01:01 +0100

    Add definition generation to components

--- game/core/src/com/lyeeedar/Components/ComponentType.kt
+++ game/core/src/com/lyeeedar/Components/ComponentType.kt
@@ -1,20 +1,19 @@
 package com.lyeeedar.Components
 
-enum class ComponentType private constructor(val constructor: ()->AbstractComponent)
+enum class ComponentType private constructor(val constructor: ()->AbstractComponent<*>)
 {
 	// Engine
 	ArchetypeBuilder({ ArchetypeBuilderComponent() }),
-	AdditionalRenderable({ AdditionalRenderableComponent() }),
-	Dialogue({ DialogueComponent() }),
-	DirectionalSprite({ DirectionalSpriteComponent() }),
+	AdditionalRenderable({ AdditionalRenderableComponent(AdditionalRenderableComponentData()) }),
+	Dialogue({ DialogueComponent(DialogueComponentData()) }),
+	DirectionalSprite({ DirectionalSpriteComponent(DirectionalSpriteComponentData()) }),
 	Event({ EventComponent() }),
 	LoadData({ LoadDataComponent() }),
 	MarkedForDeletion({ MarkedForDeletionComponent() }),
-	MetaRegion({ MetaRegionComponent() }),
-	Name({ NameComponent() }),
-	Occludes({ OccludesComponent() }),
-	Position({ PositionComponent() }),
-	Renderable({ RenderableComponent() }),
+	MetaRegion({ MetaRegionComponent(MetaRegionComponentData()) }),
+	Name({ NameComponent(NameComponentData()) }),
+	Position({ PositionComponent(PositionComponentData()) }),
+	Renderable({ RenderableComponent(RenderableComponentData()) }),
 	Transient({ TransientComponent() });
 
 	companion object
