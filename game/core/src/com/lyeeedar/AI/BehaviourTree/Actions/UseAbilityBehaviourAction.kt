ee733fd64b05b8d8ea5e1e1d3f93c2326d825234
lyeeedar <lyeeedar@gmail.com>
2020-05-06 08:44:16 +0100

    Add support for targetting to abilities. Add behaviour action for abilities.

---
+++ game/core/src/com/lyeeedar/AI/BehaviourTree/Actions/UseAbilityBehaviourAction.kt
@@ -0,0 +1,61 @@
+package com.lyeeedar.AI.BehaviourTree.Actions
+
+import com.badlogic.gdx.utils.ObjectMap
+import com.lyeeedar.AI.BehaviourTree.BehaviourTreeState
+import com.lyeeedar.AI.BehaviourTree.EvaluationState
+import com.lyeeedar.AI.BehaviourTree.Nodes.AbstractBehaviourNode
+import com.lyeeedar.AI.Tasks.TaskUseAbility
+import com.lyeeedar.Components.ability
+import com.lyeeedar.Components.position
+import com.lyeeedar.Components.task
+import com.lyeeedar.Game.AbilityData
+import com.lyeeedar.Game.Tile
+import com.lyeeedar.Util.Point
+import com.lyeeedar.Util.XmlData
+
+class UseAbilityBehaviourAction : AbstractBehaviourAction()
+{
+	lateinit var key: String
+
+	override fun evaluate(state: BehaviourTreeState): EvaluationState
+	{
+		val storedTarget = state.getData<Point>(key, 0)
+		val entity = state.entity.get() ?: return EvaluationState.FAILED
+		val pos = entity.position() ?: return EvaluationState.FAILED
+		val abilityHolder = entity.ability() ?: return EvaluationState.FAILED
+		val task = entity.task() ?: return EvaluationState.FAILED
+
+		for (ability in abilityHolder.abilities)
+		{
+			if (ability.remainingCooldown <= 0)
+			{
+				val target =
+					if (ability.data.targetType == AbilityData.TargetType.TARGET_ENEMY)
+						storedTarget
+					else
+						ability.pickTarget(entity, state.world, state.rng)
+				if (target == null) continue
+				val tile = state.world.grid.tryGet(target, null) as? Tile ?: continue
+
+				task.tasks.add(TaskUseAbility.obtain().set(tile, ability))
+
+				return EvaluationState.RUNNING
+			}
+		}
+
+		return EvaluationState.FAILED
+	}
+
+	//region generated
+	override fun load(xmlData: XmlData)
+	{
+		super.load(xmlData)
+		key = xmlData.get("Key")
+	}
+	override val classID: String = "UseAbility"
+	override fun resolve(nodes: ObjectMap<String, AbstractBehaviourNode>)
+	{
+		super.resolve(nodes)
+	}
+	//endregion
+}
--- game/core/src/com/lyeeedar/Game/Ability.kt
+++ game/core/src/com/lyeeedar/Game/Ability.kt
@@ -1,17 +1,74 @@
 package com.lyeeedar.Game
 
+import com.badlogic.gdx.utils.Array
+import com.badlogic.gdx.utils.ObjectFloatMap
 import com.lyeeedar.ActionSequence.ActionSequence
-import com.lyeeedar.Util.CompiledExpression
-import com.lyeeedar.Util.DataNeedsLocalisation
+import com.lyeeedar.Components.*
+import com.lyeeedar.Systems.AbstractTile
+import com.lyeeedar.Systems.World
+import com.lyeeedar.Util.*
 import com.lyeeedar.Util.XmlData
-import com.lyeeedar.Util.XmlDataClass
 import java.util.*
+import ktx.collections.toGdxArray
+import squidpony.squidmath.LightRNG
 
 class Ability(val data: AbilityData)
 {
 	var remainingCooldown = data.cooldown.toFloat()
 
 	var justUsed = false
+
+	fun getValidTargets(entity: Entity, world: World<*>): List<AbstractTile>
+	{
+		val pos = entity.position()!!
+		val vision = entity.addOrGet(ComponentType.Vision) as VisionComponent
+		val visiblePoints = vision.getVision(pos.x, pos.y)
+		val visibleTiles = visiblePoints.filter { it.dist(pos.position) in data.range.x..data.range.y }.mapNotNull { world.grid.tryGet(it, null) }
+
+		return when (data.targetType)
+		{
+			AbilityData.TargetType.TILE -> visibleTiles
+			AbilityData.TargetType.EMPTY_TILE -> visibleTiles.filter { it.getPassable(pos.slot, entity) }
+
+			AbilityData.TargetType.ALLY -> visibleTiles.filter { it.contents[pos.slot]?.get()?.isAllies(entity) ?: false }
+
+			AbilityData.TargetType.ANY_ENEMY, AbilityData.TargetType.TARGET_ENEMY -> visibleTiles.filter { it.contents[pos.slot]?.get()?.isEnemies(entity) ?: false }
+		}
+	}
+
+	private val tempVariableMap = ObjectFloatMap<String>()
+	private val values: Array<Pair<AbstractTile, Float>> = Array()
+	fun pickTarget(entity: Entity, world: World<*>, rng: LightRNG): AbstractTile?
+	{
+		val validTargets = getValidTargets(entity, world)
+		if (validTargets.isEmpty()) return null
+
+		if (data.targetType == AbilityData.TargetType.TILE || data.targetType == AbilityData.TargetType.EMPTY_TILE)
+		{
+			return validTargets.random(rng)
+		}
+
+		val pos = entity.position()!!
+
+		values.clear()
+		for (target in validTargets)
+		{
+			val other = target.contents[pos.slot].get()!!
+
+			tempVariableMap.clear()
+			other.statistics()!!.write(tempVariableMap)
+			tempVariableMap["dist"] = other.position()!!.position.dist(pos.position).toFloat()
+
+			if (data.targetCondition.evaluate(tempVariableMap, rng).round() != 0)
+			{
+				val value = data.sortCondition.evaluate(tempVariableMap, rng)
+				values.add(Pair(target, value))
+			}
+		}
+
+		val sorted = if (data.selectMinByCondition) values.sortedBy { it.second } else values.sortedByDescending { it.second }
+		return sorted.first().first
+	}
 }
 
 class AbilityData : XmlDataClass()
@@ -46,9 +103,18 @@ class AbilityData : XmlDataClass()
 
 	var singleUse: Boolean = false
 
-	var range: Int = 1
+	@DataVector(name1 = "Min", name2 = "Max")
+	var range: Point = Point(1,1)
 	lateinit var targetType: TargetType
+
+	@DataValue(visibleIf = "TargetType != Target_enemy && TargetType != Tile && TargetType != Empty_tile")
 	lateinit var targetCondition: CompiledExpression
+
+	@DataValue(visibleIf = "TargetType != Target_enemy && TargetType != Tile && TargetType != Empty_tile")
+	lateinit var sortCondition: CompiledExpression
+
+	@DataValue(visibleIf = "TargetType != Target_enemy && TargetType != Tile && TargetType != Empty_tile")
+	var selectMinByCondition: Boolean = true
 
 	fun get(): Ability = Ability(this)
 
@@ -63,9 +129,12 @@ class AbilityData : XmlDataClass()
 		cooldown = xmlData.getInt("Cooldown", 10)
 		cooldownType = CooldownType.valueOf(xmlData.get("CooldownType").toUpperCase(Locale.ENGLISH))
 		singleUse = xmlData.getBoolean("SingleUse", false)
-		range = xmlData.getInt("Range", 1)
+		val rangeRaw = xmlData.get("Range", "1,1")!!.split(',')
+		range = Point(rangeRaw[0].trim().toInt(), rangeRaw[1].trim().toInt())
 		targetType = TargetType.valueOf(xmlData.get("TargetType").toUpperCase(Locale.ENGLISH))
 		targetCondition = CompiledExpression(xmlData.get("TargetCondition"))
+		sortCondition = CompiledExpression(xmlData.get("SortCondition"))
+		selectMinByCondition = xmlData.getBoolean("SelectMinByCondition", true)
 	}
 	//endregion
 }
