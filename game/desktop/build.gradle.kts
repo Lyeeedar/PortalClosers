187c5123ec025f2a3ba604fe7c0646218a32ac2d
lyeeedar <lyeeedar@gmail.com>
2020-05-13 16:52:57 +0100

    Refactor particleviewer into a more generic InGamePreviewer

--- game/desktop/build.gradle.kts
+++ game/desktop/build.gradle.kts
@@ -1,3 +1,12 @@
 project.dependencies {
 	// put project dependencies in here
 }
+
+tasks.register<Jar>("mapGenerationPreviewDist") {
+	from(files(project.the<SourceSetContainer>()["main"].output.classesDirs))
+	from(configurations["runtimeClasspath"].map { if (it.isDirectory) it else zipTree(it) })
+
+	manifest {
+		attributes["Main-Class"] = "com.lyeeedar.desktop.MapGenerationPreviewLauncher"
+	}
+}
